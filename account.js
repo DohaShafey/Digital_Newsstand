47ae8ae46526abdc0bb58611bba01c051147eead
DohaShafey <doha.hamed.2006@gmail.com>
2025-05-09 21:15:08 +0300

    1

--- account.js
+++
@@ -1,8 +0,0 @@
-// زر "تعديل المعلومات"
-document.getElementById("edit-btn").addEventListener("click", function() {
-    alert("Updated Information successfuly");
-});
-
-// حالياً ما في وظائف JS مضافة، بس ممكن تضيفي حسب الحاجة
-console.log("Account page loaded.");
-
