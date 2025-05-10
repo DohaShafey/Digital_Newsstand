e93ed327c72f64ae4e16ea6cd4ba7771e65093cd
unknown <hagerhamoda788@gmail.com>
2025-05-10 10:48:59 -0700

    the user class

---
+++ Models/User.php
@@ -0,0 +1,32 @@
+<?php
+
+class User {
+    protected $userId;
+    private $userName;
+    private $userEmail;
+    private $userPassword;
+    protected $userRole;
+    protected $languageId;
+    
+    public function setUserEmail($userEmail){
+        $this->$userEmail = $userEmail;
+    }
+
+    public function setUserPassword($userPassword){
+        $this->$userPassword = $userPassword;
+    }
+
+    public function getUserEmail(){
+        return $this->userEmail;
+    }   
+
+    public function getUserPassword(){
+        return $this->userPassword;
+    }
+
+    public function getUserId(){
+        return $this->userId;
+    }   
+}
+
+?>
